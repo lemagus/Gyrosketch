b8fed29566ebf3e884351f37a5224971c985047a
Goyza II <goyza@Goyza.local>
2017-03-30 15:53:01 +0200

    Draw with random colors

--- js/index.js
+++ js/index.js
@@ -1,12 +1,33 @@
+var paper = $('#paper');
+paper.attr('width', $(window).width());
+paper.attr('height', $(window).height());
+
+var ctx = paper.get(0).getContext('2d');
+
+var drawCircle = function(pos) {
+	
+	var color = $('div.ball').css('background-color');
+	
+	ctx.fillStyle = color;
+		
+	ctx.beginPath();
+	ctx.arc(pos.left,pos.top, $('div.ball').width()/2 , 0 , 2*Math.PI );
+	ctx.closePath();
+	ctx.fill();
+	
+}
+
 $(document).ready( function(){
 	
-	gyro.frequency = 100;
+	var fps = 120;
+	
+	gyro.frequency = Math.round(1000 / fps);
 	
 	gyro.startTracking(function(o) {
 		
 		//$('textarea').html( JSON.stringify(o) );
 		
-		var speed = 15;
+		var speed = 0.2;
 		
 		var axisXDirection = o.gamma >= 0 ? 1 : -1;
 		var axisYDirection = o.beta >= 0 ? -1 : 1;
@@ -23,15 +44,18 @@ $(document).ready( function(){
 		topX = Math.max( $('div.ball').width()/2 , topX);
 		topX = Math.min( $(window).width() - $('div.ball').width()/2 , topX);
 		
-		topY = Math.max(0, topY);
-		topY = Math.min( $(window).height() , topY);
+		topY = Math.max( $('div.ball').height()/2, topY);
+		topY = Math.min( $(window).height() - $('div.ball').height()/2 , topY);
 		
 		var pos = {
 			'left' : topX,
 			'top' : topY
 		};
 		
-		$('div.ball').stop().animate(pos, 500 , 'linear');
+		drawCircle(pos);
+		
+		//$('div.ball').stop().animate(pos, 500 , 'linear');
+		$('div.ball').css(pos);
 		
 		pos['axisX'] = axisXDirection;
 		pos['axisY'] = axisYDirection;
@@ -45,7 +69,23 @@ $(document).ready( function(){
 		pos['progressX'] = progressX;
 		pos['progressY'] = progressY;
 		
-		$('textarea').html( JSON.stringify(pos) );
-		
 	});
+	
+	$('body').on('touchstart', function(){
+		
+		var r = Math.round(Math.random() * 255 );
+		var g = Math.round(Math.random() * 255 );
+		var b = Math.round(Math.random() * 255 );
+		
+		$('div.ball').css('background-color', 'rgb(' + r + ',' + g + ','+ b  +')');
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
 });
